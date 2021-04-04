6086c99d9cdec75baf0676d6bbd764511266de65
walsc024 <chriswalsh08@googlemail.com>
2021-04-04 18:36:53 +0100

    added elemetns to render

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from './Navbar';
+import React from 'react';
+import './App.css';
+
+import Header from './components/Header';
+import About from './components/About';
+import Work from './components/work';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar'
 
 function App() {
-    return(
-        <Navbar />
-    );
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <div className="App-header">
+      <Header></Header>
+      </div>
+      
+      <div className='About'>
+        <About></About>
+      </div>
+      <div className='work'>
+        <Work></Work>
+      </div>
+      <div className='contact'>
+        <Contact></Contact>
+      </div>
+    </div>
+  );
 }
-export default App; 
+
+export default App;
