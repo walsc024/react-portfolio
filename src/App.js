45aeba00dc261f8f10795024267e3f57aa0e9f74
walsc024 <chriswalsh08@googlemail.com>
2021-04-04 19:23:04 +0100

    css changes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
-import About from './components/About';
 import Work from './components/work';
-import Contact from './components/Contact';
 import Navbar from './components/Navbar'
 
 function App() {
@@ -14,16 +12,10 @@ function App() {
       <div className="App-header">
       <Header></Header>
       </div>
-      
-      <div className='About'>
-        <About></About>
-      </div>
       <div className='work'>
         <Work></Work>
       </div>
-      <div className='contact'>
-        <Contact></Contact>
-      </div>
+  
     </div>
   );
 }
