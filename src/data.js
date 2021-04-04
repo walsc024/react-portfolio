bafb0537457cfb55751d55d649a4f7003385d7f7
walsc024 <chriswalsh08@googlemail.com>
2021-04-04 18:41:34 +0100

    added data file and the data

---
+++ src/data.js
@@ -0,0 +1,74 @@
+export default
+    {
+        
+        name :'Christopher Neill',
+        headerTagline: [
+                        'I am Christopher Neill.',
+                        'a full stack web developer',
+                        'from Austin'
+    ],
+        contactEmail:'christopherneill2019@icloud.com',
+        resume: "./resume.zip",
+        abouttext: "I am a well organized people person who is focused on excellent client support, program development, and complex problem solving. Passionate about helping others. Cultivate positive morale by coaching and encouraging others.",
+        aboutImage:'./backgpic.jpeg',
+       ShowAboutImage:true,
+ 
+       projects:[
+        {
+            id: 1,
+            title: 'Design A Date',
+            service: 'A program to make dating easier',
+            imageSrc: "./design.png",
+            url: 'https://christopherneill.github.io/GroupProject/'
+        },
+        {
+            id: 2,
+            title: 'Find a Lost pet',
+            service: 'microchip matcher',
+            imageSrc: "./catsndogs.png",
+            url: 'https://polar-stream-66961.herokuapp.com/'
+        },
+
+           {
+            id: 3,
+            title:'Password Generator', 
+             service:'First use of javascript', 
+             imageSrc:"./pass.png",
+             url:'https://christopherneill.github.io/javascript/'
+            },
+            {
+                id: 4,
+                title: 'Command line inteface',
+                service: 'Node Program Development',
+                imageSrc: "./rmgen.png",
+                url: 'https://github.com/ChristopherNeill/clirmgen'
+            },
+            { 
+                id: 5,
+                title: 'Eat da burger',
+                service: 'MySQL, Node, Express, Handlebars and a ORM',
+                imageSrc: "./burger.png",
+                url: 'https://burgermonster123.herokuapp.com/'
+            },
+            {
+                id: 6,
+                title: 'Weather App',
+                service: 'Api implementation',
+                imageSrc: "./weather.png",
+                url: 'https://christopherneill.github.io/weatherapp2/'
+           }
+
+                        
+                
+               
+        ],
+        social: [
+
+            {   name:'Github',
+                url:'https://github.com/walsc024/'},
+            {
+                name: 'Linkedin',
+                url: 'https://www.linkedin.com/in/chris-walsh-94455392/'
+            }
+        ]
+    }
